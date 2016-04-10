a431739573c6e1c8b4935fa387bab9df931e6d43
sofiangrh <137006200@student.unsil.ac.id>
2016-04-10 22:00:52 +0700

    Create blas.js

---
+++ blas.js
@@ -0,0 +1,23 @@
+const writeArea = document.querySelector('.js-write-area')
+const listOfSaves = document.querySelector('.js-list-of-saves')
+const saveNameFixed = 'scribbleSave'
+const currentSave = function () {
+  return localStorage.getItem('currentScribbleSave')
+}
+
+const loadSaves = function () {
+  return Object.keys(localStorage).filter(function (key) {
+    return key.startsWith(saveNameFixed)
+  }).reverse()
+}
+
+const loadSave = function (saveName) {
+  if (localStorage.getItem(saveName) !== null) {
+    setCurrentSave(saveName)
+    writeArea.value = localStorage.getItem(saveName)
+    renderListing()
+    writeArea.focus()
+  } else {
+    alert('save not found')
+  }
+}
